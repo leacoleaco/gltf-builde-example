371ed509b6578dc759e4c10b08dd4253ecaddbec
like05 <like05@longfor.com>
2022-01-26 17:00:50 +0800

    upgrade

--- src/test/kotlin/pro/leaco/gltf/MeshBuilderExtKtTest.kt
+++ src/test/kotlin/pro/leaco/gltf/MeshBuilderExtKtTest.kt
@@ -32,4 +32,34 @@ class MeshBuilderExtKtTest {
 
     }
 
+
+    @Test
+    fun testAddGeometryPlaneTrans() {
+
+        val builder = GltfBuilder()
+            // set the path used for finding textures
+            .withBasePath(File("src/test/resources"))
+
+        // create materials for each of the textures
+        val kineticaMaterial: Material = builder.newTextureMaterial("kinetica_logo.png")
+        val gltfMaterial: Material = builder.newTextureMaterial("gltf_logo.png")
+        val uvGridMaterial: Material = builder.newTextureMaterial("uv_grid_512.png")
+
+        val geom =
+            GeometryMaker.parse("POLYGON ((90 370, 300 370, 300 120, 90 120, 90 370), \n" + "  (180 290, 150 200, 250 200, 185 226, 180 290))")
+
+        val node = builder.buildNodeWith(
+            MeshBuilder("face-XY1")
+                .withMaterial(kineticaMaterial)
+                .addGeometryPlane(geom, true)
+        )
+
+        node.rotation = floatArrayOf(0.7f, 0f, 0.7f, 0f)
+
+
+        val outFile: File = TestShapeModels.getFile("test_geom_transform")
+        builder.writeGltf(outFile)
+
+    }
+
 }
